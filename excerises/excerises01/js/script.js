331892dcbf9a96e924a9e4ce966bc095b9d6a53f
hellojademo <jade.mo@outlook.com>
2017-01-14 18:30:35 -0500

    changed the hover function and got rid of filler text

--- excerises/excerises01/js/script.js
+++ excerises/excerises01/js/script.js
@@ -1,39 +1,21 @@
-/*
 
-DART450
-Week 01
-Colour Change
-
-Just change the colour of the text to red if it is clicked using jQuery's
-.css() function
-
-*/
-
-// We use "document ready" so that our code only runs once the whole
-// webpage is loaded and ready.
 
 $(document).ready(function() {
 
-  // We use the jQuery .click() function to "listen" for clicks on our
-  // div with id "text"
-  //
-  // $('#text').click(function () {
-  //
-  //   // This is what we do when it is clicked
-  //
-  //   // Inside the function "this" refers to the thing that got clicked
-  //   // And we use .css() to set its CSS color property to ""
-  //   $(this).css('color','pink');
-  //
 
 	$("#text").click(function(){
 			$(this).animate({right:'+=2%',opacity:'.75', padding:'+=10px',}, 10);
 		});
 
-    // What would happen if we didn't use "this" and used "#text" instead?
 
-    // What if there was more than one "#text" id in the HTML?
+	  $('.cake').hover(function () {
+
+	    $(this).fadeTo(500,0.2);
+
+	  },function () {
+  				$(this).fadeTo(500,1);
 
 
+	  });
 
 });
