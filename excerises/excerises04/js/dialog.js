d0534e2b3ffd87939e5787c87f29a4ec4e5ed7cc
JADE MORGAN <jad_morg@cda027ev5709x.local>
2017-02-07 18:00:32 -0500

    i did the thignfnsjsd

---
+++ excerises/excerises04/js/dialog.js
@@ -0,0 +1,43 @@
+// This file defines a variable with our dialog in it
+//
+// Each element in the dialog array is an object with two properties:
+//
+//    'name' contains the name of the person saying the line
+//     and conveniently translates to the name of the CSS id for that
+//     character.
+//
+//    'line' contains the line of dialog for them to speak
+//
+// The elements are in the array IN THE ORDER that they should be spoken
+// by the characters.
+
+var dialog = [
+  {
+    name: "juliet",
+    line: "Romeo!"
+  },
+  {
+    name: "romeo",
+    line: "My dear?"
+  },
+  {
+    name: "juliet",
+    line: "At what o'clock to-morrow shall I send to thee?"
+  },
+  {
+    name: "romeo",
+    line: "At the hour of nine."
+  },
+  {
+    name: "juliet",
+    line: "I will not fail: 'tis twenty years till then."
+  },
+  {
+    name: "juliet",
+    line: "I have forgot why I did call thee back."
+  },
+  {
+    name: "romeo",
+    line: "Touch Me"
+  }
+];
